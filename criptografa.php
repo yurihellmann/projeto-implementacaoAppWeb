ed92612d3e49e681233926557d3f80b0b5da8d09
yurihellmann <yurihellmann88@gmail.com>
2024-07-03 08:11:54 -0300

    Update criptografa.php

--- criptografa.php
+++ criptografa.php
@@ -1,5 +1,5 @@
 <?php
-$senha = 'Earth@1999'; // Altere para a senha desejada
+$senha = 'admin'; // Altere para a senha desejada
 $hashed_senha = password_hash($senha, PASSWORD_BCRYPT);
 echo 'Senha Hashed: ' . $hashed_senha;
 ?>
