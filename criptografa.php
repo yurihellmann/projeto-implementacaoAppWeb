38c95ab5ce7015da4952b0f5fbf0ec9460b4a7b5
yurihellmann <yurihellmann88@gmail.com>
2024-07-03 16:56:08 -0300

    comentários criptografa

--- criptografa.php
+++ criptografa.php
@@ -1,5 +1,5 @@
 <?php
-$senha = 'admin'; // Altere para a senha desejada
-$hashed_senha = password_hash($senha, PASSWORD_BCRYPT);
-echo 'Senha Hashed: ' . $hashed_senha;
+$senha = 'admin'; //recebe a senha - inserção manual
+$hashed_senha = password_hash($senha, PASSWORD_BCRYPT); //utiliza a função do PHP e recebe dois parametros, a senha e o metodo a ser utilizada no criptografia
+echo 'Senha Hashed: ' . $hashed_senha; //imprime na tela o hash da senha que deve ser salvo na banco de dados
 ?>
